93e7e8888d61110f1c3666491f57707feb071323
mINFLEE <maxkotovych@gmail.com>
2024-10-27 18:09:36 +0300

    second commit

--- Lab_03_4/Lab_03_4.cpp
+++ Lab_03_4/Lab_03_4.cpp
@@ -17,12 +17,12 @@ int main()
     cout << "x = "; cin >> x;
     cout << "y = "; cin >> y;
 
-    if ((y <= (x - 2) * (x - 2) - 3 && y >= abs(x) && x >= 0) ||
-        (y >= (x - 2) * (x - 2) - 3 && y <= abs(x) && x <= 0))
-        cout << "yes" << endl;
-    else
+    if ((y >= (x - 2) * (x - 2) - 3 && y >= abs(x)) ||
+        (y >= (x - 2) * (x - 2) - 3 && y >= abs(x) && x <= 0))  
+        cout << "yes" << endl;  
+    else 
         cout << "no" << endl;
-
+    
     cin.get();
     return 0;
 }
